7ec047d6f4d263bc2ef35ba5f025ebbffdcda82e
ErickPCPOSGRADO <erick.rodriguezmo@uanl.edu.mx>
2025-02-17 13:06:03 -0600

    Clase del 17/02/25 Pruebas de normalidad Homogeneidad de varianzas Prueba de t (t.test) Dependiente e independiente.

--- Clase 3.R
+++ Clase 3.R
@@ -62,3 +62,59 @@ boxplot(vivero$IE ~ vivero$Tratamiento, xlab="Tratamientos", ylab="IE")
 
 #Alfa. Límite. 0.05. Con un 95% de confiabilidad en el resultado.
 # p-value. Contrastar con Alfa. -1 a 1. # ¿Qué pedo con p-value y alfa.
+
+
+
+
+# Clase 17/02/25
+# Alfa
+# Menos espacio para la hipótesis alternativa.
+# El mainspace es para la hipótesis nula.
+# Por default viene para dos colas
+# Si hay diferencia o no (dos colas)
+# Especificaciones para (mi media es mayor, o menos qué) (una cola)
+
+# SHAPIRO.TEST
+
+
+
+
+# Revisar normalidad de los datos
+shapiro.test(vivero$IE)
+# p es el valor que contrastaré contra el valor de 0.05 (normalmente, dependiendo el valor de alfa)
+# la prueba de normalidad dice la Ho que son datos normales, y la H1 que NO son normales. 
+# Si p es mayor a 0.05, significa que entra en el área de Ho.
+# Para este caso, la normalidad de vivero$IE es de 0.1777, lo que hace que se pase de 0.05 y entre a la hipótesis nula.
+# Se leen de derecha a izquierda.
+# Tienen que tener al menos 30 datos. 
+# LA TEORÍA DEL LÍMITE CENTRAL de que todos los datos de la naturaleza cumplen con una normalidad. 
+
+
+# Cualquier prueba estadística te brindará un valor de p
+# En pruebas de dos colas, los límites para el mismo valor de alfa(0.95) son de 0.025 y están en ambos lados,
+# siendo el de la izquierda negativo y el derecho positivo, el centro es el 0. 
+# Importante diferenciar qué tipo de análisis se tiene para entender si, por ejemplo, un valor de 0.049 es aceptar la H1 en una cola, o la H0 en dos colas.
+?subset
+ctrl <- subset(vivero$IE, vivero$Tratamiento == "Ctrl")
+fert <- subset(vivero$IE, vivero$Tratamiento != "Ctrl")
+var(fert)
+var(ctrl)
+
+# Revisar homogeneidad de varianzas
+bartlett.test(vivero$IE ~ vivero$Tratamiento)
+#Las varianzas son iguales.
+
+# Tenemos homogeneidad en las varianzas y normalidad de datos
+
+# Prueba de t de dos muestras independientes
+t.test(vivero$IE~ vivero$Tratamiento, var.equal=T)
+# t tab se contrasta con t cal.
+# Los grados de libertad (numero de datos totales  - tratamientos)
+# Cuando los grados de libertad no son enteros se está usando la prueba con las varianzas no iguales.
+
+#Para indicar si la prueba de t es con variables dependientes se agrega paired = T en la prueba de tes.
+# t.test(vivero$IE ~ vivero$Tratamiento, paired = T)
+t.test(vivero$IE ~ vivero$Tratamiento, paired = T)
+
+# La diferencia en los grados de libertad que salen con ambos, es que en la prueba independitente te considera, en este caso, que hay 42 individuos
+# que se dividen en dos tratamientos. Y la prueba dependiente considera que hay 21 individuos que se han medido dos veces, con diferencia en el tiempo.
